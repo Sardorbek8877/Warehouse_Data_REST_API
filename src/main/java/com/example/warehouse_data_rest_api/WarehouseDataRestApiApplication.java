99930b40e5a3e28dec32a4ebcc35315f870c5431
Sardorbek8877 <sardorbeksafarov8844@gmail.com>
2023-06-10 16:47:29 +0200

    create Project

---
+++ src/main/java/com/example/warehouse_data_rest_api/WarehouseDataRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.warehouse_data_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseDataRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseDataRestApiApplication.class, args);
+    }
+
+}
